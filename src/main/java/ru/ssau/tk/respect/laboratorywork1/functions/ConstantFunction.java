ec024e79c1f0f272e9fef0fc4b7d181693d51d42
Khlebodarov <andrw.h2002@mail.ru>
2021-09-22 07:01:01 +0300

    Добавлен класс ConstantFunction

---
+++ src/main/java/ru/ssau/tk/respect/laboratorywork1/functions/ConstantFunction.java
@@ -0,0 +1,17 @@
+package ru.ssau.tk.respect.laboratorywork1.functions;
+
+public class ConstantFunction implements MathFunction {
+    private final double CONSTANT;
+
+    public ConstantFunction(double CONSTANT) {
+        this.CONSTANT = CONSTANT;
+    }
+
+    public double getCONSTANT() {
+        return CONSTANT;
+    }
+
+    public double apply(double x) {
+        return CONSTANT;
+    }
+}
