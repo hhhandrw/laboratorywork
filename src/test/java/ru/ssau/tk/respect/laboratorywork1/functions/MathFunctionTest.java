c247c64ee1e34b913338699b368b6c2db6afe341
Khlebodarov <andrw.h2002@mail.ru>
2021-09-22 08:12:45 +0300

    Метод andThen покрыт тестами

---
+++ src/test/java/ru/ssau/tk/respect/laboratorywork1/functions/MathFunctionTest.java
@@ -0,0 +1,23 @@
+package ru.ssau.tk.respect.laboratorywork1.functions;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class MathFunctionTest {
+
+    @Test
+    public void testAndThen() {
+        MathFunction sf = new SqrFunction();
+        MathFunction tf = new TanFunction();
+        MathFunction id = new IdentityFunction();
+        assertEquals(sf.andThen(tf).andThen(id).apply(1),
+                1.5574077246549023);
+        assertEquals(tf.andThen(sf).andThen(id).apply(8),
+                46.23607587425524);
+        assertEquals(id.andThen(tf).andThen(sf).apply(17),
+                12.207446537693869);
+        assertEquals(id.andThen(sf).andThen(tf).apply(10),
+                -0.5872139151569291);
+    }
+}
