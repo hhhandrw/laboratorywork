c952f9518092beaf8c8b038dd68e74e7a871517c
Khlebodarov <andrw.h2002@mail.ru>
2021-09-22 04:31:56 +0300

    Класс IdentityFunction покрыт тестами

---
+++ src/test/java/ru/ssau/tk/respect/laboratorywork1/functions/IdentityFunctionTest.java
@@ -0,0 +1,17 @@
+package ru.ssau.tk.respect.laboratorywork1.functions;
+
+import org.testng.annotations.*;
+
+import static org.testng.Assert.*;
+
+public class IdentityFunctionTest {
+    @Test
+    public void TestFunction() {
+        IdentityFunction function = new IdentityFunction();
+
+        assertEquals(function.apply(5), 5.0);
+        assertEquals(function.apply(0), 0.0);
+        assertEquals(function.apply(-14), -14.0);
+        assertEquals(function.apply(1 / 20.0), 0.05);
+    }
+}
