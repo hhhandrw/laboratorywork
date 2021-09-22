e3d0a632eab4402e16234e5595c0152b58af2566
Khlebodarov <andrw.h2002@mail.ru>
2021-09-22 04:12:17 +0300

    Добавлен класс IdentityFunction

---
+++ src/main/java/ru/ssau/tk/respect/laboratorywork1/functions/IdentityFunction.java
@@ -0,0 +1,8 @@
+package ru.ssau.tk.respect.laboratorywork1.functions;
+
+public class IdentityFunction implements MathFunction {
+    @Override
+    public double apply(double x) {
+        return x;
+    }
+}
