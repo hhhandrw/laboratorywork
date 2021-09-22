5e18be4fe3a0f806f0ed1ba8a1a9f2bd90e74702
Khlebodarov <andrw.h2002@mail.ru>
2021-09-22 04:55:11 +0300

    Добавлен класс TanFunction (вычисление тангенса)

---
+++ src/main/java/ru/ssau/tk/respect/laboratorywork1/functions/TanFunction.java
@@ -0,0 +1,9 @@
+package ru.ssau.tk.respect.laboratorywork1.functions;
+
+public class TanFunction implements MathFunction {
+    @Override
+    public double apply(double x) {
+        x=Math.tan(x);
+        return x;
+    }
+}
