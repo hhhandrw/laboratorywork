0ca67d9260b4da0cf1b3cbcb99d4e3c9e32c9a6a
Khlebodarov <andrw.h2002@mail.ru>
2021-09-22 05:18:46 +0300

    Класс TanFunction покрыт тестами + некоторые файлы автоматически отформатированы

--- src/main/java/ru/ssau/tk/respect/laboratorywork1/functions/SqrFunction.java
+++ src/main/java/ru/ssau/tk/respect/laboratorywork1/functions/SqrFunction.java
@@ -1,9 +1,9 @@
 package ru.ssau.tk.respect.laboratorywork1.functions;
 
-public class SqrFunction implements MathFunction{
+public class SqrFunction implements MathFunction {
     @Override
     public double apply(double x) {
-        x=Math.pow(x,2);
+        x = Math.pow(x, 2);
         return x;
     }
 }
--- src/main/java/ru/ssau/tk/respect/laboratorywork1/functions/TanFunction.java
+++ src/main/java/ru/ssau/tk/respect/laboratorywork1/functions/TanFunction.java
@@ -3,7 +3,7 @@ package ru.ssau.tk.respect.laboratorywork1.functions;
 public class TanFunction implements MathFunction {
     @Override
     public double apply(double x) {
-        x=Math.tan(x);
+        x = Math.tan(x);
         return x;
     }
 }
