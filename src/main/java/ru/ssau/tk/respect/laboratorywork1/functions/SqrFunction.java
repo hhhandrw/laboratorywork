4ad4dcc47dac433bef52e287e784304b933310bb
Khlebodarov <andrw.h2002@mail.ru>
2021-09-22 04:43:26 +0300

    Добавлен класс SqrFunction

---
+++ src/main/java/ru/ssau/tk/respect/laboratorywork1/functions/SqrFunction.java
@@ -0,0 +1,9 @@
+package ru.ssau.tk.respect.laboratorywork1.functions;
+
+public class SqrFunction implements MathFunction{
+    @Override
+    public double apply(double x) {
+        x=Math.pow(x,2);
+        return x;
+    }
+}
