b784c545fb9d32236f204129155ba2349f4e9a78
Khlebodarov <andrw.h2002@mail.ru>
2021-09-22 07:21:16 +0300

    Класс ZeroFunction покрыт тестами

---
+++ src/test/java/ru/ssau/tk/respect/laboratorywork1/functions/ZeroFunctionTest.java
@@ -0,0 +1,19 @@
+package ru.ssau.tk.respect.laboratorywork1.functions;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class ZeroFunctionTest {
+    @Test
+    public void testApply() {
+
+        ZeroFunction z = new ZeroFunction();
+
+        assertEquals(z.apply(11), 0.0);
+
+        assertEquals(z.apply(0), 0.0);
+
+        assertEquals(z.apply(678), 0.0);
+    }
+}
