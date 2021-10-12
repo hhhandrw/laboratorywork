9e4b97206b6666aa8d6b615ba6c5f4de86e642a1
Khlebodarov <andrw.h2002@mail.ru>
2021-10-12 21:09:58 +0400

    Класс Node перенесёен в класс LinkedListTabulatedFunction

--- src/main/java/ru/ssau/tk/respect/laboratorywork1/functions/LinkedListTabulatedFunction.java
+++ src/main/java/ru/ssau/tk/respect/laboratorywork1/functions/LinkedListTabulatedFunction.java
@@ -4,6 +4,13 @@ public class LinkedListTabulatedFunction extends AbstractTabulatedFunction {
 
     private Node head;
 
+    public static class Node {
+        public Node next;
+        public Node prev;
+        public double x;
+        public double y;
+    }
+
     public LinkedListTabulatedFunction(double[] xValues, double[] yValues) {
         for (int i = 0; i < xValues.length; i++) {
             this.addNode(xValues[i], yValues[i]);
--- src/main/java/ru/ssau/tk/respect/laboratorywork1/functions/Node.java
+++
@@ -1,8 +0,0 @@
-package ru.ssau.tk.respect.laboratorywork1.functions;
-
-class Node {
-    public Node next;
-    public Node prev;
-    public double x;
-    public double y;
-}
