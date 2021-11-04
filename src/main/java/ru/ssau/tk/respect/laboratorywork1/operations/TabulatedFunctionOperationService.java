6e1fdd560f75a14b19b273073ad1c53f0233fc92
Khlebodarov <andrw.h2002@mail.ru>
2021-11-04 22:13:38 +0400

    ЛР 3

--- src/main/java/ru/ssau/tk/respect/laboratorywork1/operations/TabulatedFunctionOperationService.java
+++ src/main/java/ru/ssau/tk/respect/laboratorywork1/operations/TabulatedFunctionOperationService.java
@@ -7,6 +7,7 @@ public class TabulatedFunctionOperationService {
 
     public static Point[] asPoints(TabulatedFunction tabulatedFunction) {
         Point[] points = new Point[tabulatedFunction.getCount()];
+
         int i = 0;
         for (Point point : tabulatedFunction) {
             points[i] = point;
