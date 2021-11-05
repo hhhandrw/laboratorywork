c544a4f0fc1bfb905a91b4d2d1f50af6635288a4
Khlebodarov <andrw.h2002@mail.ru>
2021-11-05 16:32:09 +0400

    добавлен метод void writeTabulatedFunction(BufferedOutputStream outputStream, TabulatedFunction function)

--- src/main/java/ru/ssau/tk/respect/laboratorywork1/io/FunctionsIO.java
+++ src/main/java/ru/ssau/tk/respect/laboratorywork1/io/FunctionsIO.java
@@ -1,7 +1,22 @@
 package ru.ssau.tk.respect.laboratorywork1.io;
 
+import ru.ssau.tk.respect.laboratorywork1.functions.Point;
+import ru.ssau.tk.respect.laboratorywork1.functions.TabulatedFunction;
+
+import java.io.*;
+
 public final class FunctionsIO {
     private FunctionsIO () {
         throw new UnsupportedOperationException();
+    }
+
+    static void writeTabulatedFunction(BufferedOutputStream outputStream, TabulatedFunction function) throws IOException {
+        DataOutputStream out = new DataOutputStream(outputStream);
+        out.writeInt(function.getCount());
+        for (Point point: function) {
+            out.writeDouble(point.x);
+            out.writeDouble(point.y);
+        }
+        out.flush();
     }
 }
