11db64807479a8b8bde69d26a2799d41acc2caa4
Khlebodarov <andrw.h2002@mail.ru>
2021-11-08 17:47:37 +0400

    Добавлены методы TabulatedFunction readTabulatedFunction(BufferedReader reader, TabulatedFunctionFactory factory) и TabulatedFunction readTabulatedFunction(BufferedInputStream inputStream, TabulatedFunctionFactory factory)

--- src/main/java/ru/ssau/tk/respect/laboratorywork1/io/FunctionsIO.java
+++ src/main/java/ru/ssau/tk/respect/laboratorywork1/io/FunctionsIO.java
@@ -2,10 +2,15 @@ package ru.ssau.tk.respect.laboratorywork1.io;
 
 import ru.ssau.tk.respect.laboratorywork1.functions.Point;
 import ru.ssau.tk.respect.laboratorywork1.functions.TabulatedFunction;
+import ru.ssau.tk.respect.laboratorywork1.functions.factory.TabulatedFunctionFactory;
 
 import java.io.*;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Locale;
 
 public final class FunctionsIO {
+
     private FunctionsIO() {
         throw new UnsupportedOperationException();
     }
@@ -18,5 +23,37 @@ public final class FunctionsIO {
             out.writeDouble(point.y);
         }
         out.flush();
+    }
+
+    static TabulatedFunction readTabulatedFunction(BufferedInputStream inputStream, TabulatedFunctionFactory factory) throws IOException {
+        DataInputStream in = new DataInputStream(inputStream);
+        int length = in.readInt();
+
+        double[] xValues = new double[length];
+        double[] yValues = new double[length];
+
+        for (int i = 0; i < length; i++) {
+            xValues[i] = in.readDouble();
+            yValues[i] = in.readDouble();
+        }
+        return factory.create(xValues, yValues);
+    }
+
+    static TabulatedFunction readTabulatedFunction(BufferedReader reader, TabulatedFunctionFactory factory) throws IOException {
+        int count = Integer.parseInt(reader.readLine());
+        double[] xValues = new double[count];
+        double[] yValues = new double[count];
+        NumberFormat numberFormat = NumberFormat.getInstance(Locale.forLanguageTag("ru"));
+
+        for (int i = 0; i < count; i++) {
+            String str = reader.readLine();
+            try {
+                xValues[i] = numberFormat.parse(str.split(" ")[0]).doubleValue();
+                yValues[i] = numberFormat.parse(str.split(" ")[1]).doubleValue();
+            } catch (ParseException parseException) {
+                throw new IOException(parseException);
+            }
+        }
+        return factory.create(xValues, yValues);
     }
 }
