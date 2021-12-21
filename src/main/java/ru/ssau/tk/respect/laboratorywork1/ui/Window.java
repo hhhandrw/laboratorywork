8abed95bc4c1f1f2536cc64daa7426beeb15180c
Khlebodarov <andrw.h2002@mail.ru>
2021-12-22 02:48:07 +0400

    ЛР 5

--- src/main/java/ru/ssau/tk/respect/laboratorywork1/ui/Window.java
+++ src/main/java/ru/ssau/tk/respect/laboratorywork1/ui/Window.java
@@ -9,6 +9,8 @@ import javax.swing.table.AbstractTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,10 +23,9 @@ public class Window extends JFrame {
     JTable table = new JTable(tableModel);
     JLabel label = new JLabel("Количество точек:");
 
-    JTextField textField = new JTextField("");
+    JTextField textField = new JTextField("2");
     JButton addButton = new JButton("Добавить");
     JButton createButton = new JButton("Создать");
-    JButton cancelButton = new JButton("Отмена");
     JButton refreshButton = new JButton("Очистить");
 
     public Window() {
@@ -34,7 +35,6 @@ public class Window extends JFrame {
         setSize(500, 300);
 
         addButton.setFocusPainted(false);
-        cancelButton.setFocusPainted(false);
         refreshButton.setFocusPainted(false);
         createButton.setFocusPainted(false);
 
@@ -42,7 +42,6 @@ public class Window extends JFrame {
         getContentPane().add(textField);
         getContentPane().add(addButton);
         getContentPane().add(createButton);
-        getContentPane().add(cancelButton);
         getContentPane().add(refreshButton);
 
         table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
@@ -64,7 +63,7 @@ public class Window extends JFrame {
                         .addComponent(label)
                         .addComponent(textField)
                         .addComponent(addButton)
-                        .addComponent(cancelButton))
+                )
                 .addComponent(tableScrollPane)
                 .addGroup(layout.createSequentialGroup()
                         .addComponent(createButton)
@@ -77,7 +76,7 @@ public class Window extends JFrame {
                         .addComponent(label)
                         .addComponent(textField)
                         .addComponent(addButton)
-                        .addComponent(cancelButton))
+                )
                 .addComponent(tableScrollPane)
                 .addGroup(layout.createParallelGroup()
                         .addComponent(createButton)
@@ -99,12 +98,14 @@ public class Window extends JFrame {
                         xValues.add(" ");
                         yValues.add(" ");
                     }
+                    refreshButton.setEnabled(true);
                     tableModel.fireTableDataChanged();
                 } catch (NumberFormatException exception) {
                     ExceptionHandler.showMessage("Введите целое число.");
                 }
             }
         }
+        refreshButton.setEnabled(false);
         textField.addActionListener(new AddingAction());
         addButton.addActionListener(new AddingAction());
         createButton.addActionListener(e -> {
@@ -121,18 +122,77 @@ public class Window extends JFrame {
                 System.out.println(function);
                 dispose();
             } catch (NumberFormatException exp) {
-                ExceptionHandler.showMessage("Введите число в виде десятичной дроби через точку.");
+                ExceptionHandler.showMessage("Введите число в виде десятичной дроби через точку");
             } catch (ArrayIsNotSortedException exp) {
-                ExceptionHandler.showMessage("Некорректные данные: значения X должны располагаться по возрастанию.");
+                ExceptionHandler.showMessage("Некорректные данные: значения X должны располагаться по возрастанию");
             } catch (IllegalArgumentException exp) {
-                ExceptionHandler.showMessage("Невозможно создать функцию менее чем из двух точек.");
+                ExceptionHandler.showMessage("Невозможно создать функцию менее чем из двух точек");
             }
         });
 
-        cancelButton.addActionListener(e -> {
+        refreshButton.addActionListener(e -> {
+            int flag = JOptionPane.showConfirmDialog(null, "Вы уверены?", "Предупреждение", JOptionPane.YES_NO_OPTION);
+            if (flag == 0) {
+                int size = xValues.size();
+                xValues.clear();
+                yValues.clear();
+                for (int i = 0; i < size; i++) {
+                    xValues.add(" ");
+                    yValues.add(" ");
+                }
+                tableModel.fireTableDataChanged();
+            }
+        });
+
+        table.addMouseListener(new MouseListener() {
+            public void mouseClicked(MouseEvent e) {
+                if (e.getButton() == MouseEvent.BUTTON3) {
+                    int selectedRow = table.getSelectedRow();
+                    if (selectedRow != -1) {
+                        getPopupMenu().show(e.getComponent(), e.getX(), e.getY());
+                    }
+                }
+            }
+            private JPopupMenu getPopupMenu() {
+                JPopupMenu popupMenu = new JPopupMenu();
+                JMenuItem deleteItem = new JMenuItem("Удалить");
+                JMenuItem cleanItem = new JMenuItem("Очистить");
+                deleteItem.addActionListener(e -> {
+                    xValues.remove(table.getSelectedRow());
+                    yValues.remove(table.getSelectedRow());
+                    if (xValues.isEmpty()) {
+                        refreshButton.setEnabled(false);
+                    }
+                    tableModel.fireTableDataChanged();
+                });
+                cleanItem.addActionListener(e -> {
+                    xValues.set(table.getSelectedRow(), " ");
+                    yValues.set(table.getSelectedRow(), " ");
+                    tableModel.fireTableDataChanged();
+                });
+                popupMenu.add(deleteItem);
+                popupMenu.add(cleanItem);
+                return popupMenu;
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+            }
+
         });
     }
-
 
     public static void main(String[] args) {
         new Window();
